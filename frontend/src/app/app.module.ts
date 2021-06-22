11ca3ecc5183bbbac4e8aa62fadc2645ee603bea
Idon Care <tristan.wolf@rub.de>
2021-06-22 17:28:14 +0200

    :sparkles: Adding login and chat component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
+import {Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent, canActivate: [AuthGuard]}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ChatWindowComponent
   ],
   imports: [
     BrowserModule
